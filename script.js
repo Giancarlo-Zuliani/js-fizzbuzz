a41df403e25f8fedd1ea9c316450cfec716bde89
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-26 14:03:29 +0100

    code clean

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (i = 1 ; i <= nums ; i++){
    if( i % 3 === 0 && i % 5 === 0 ){
      console.log("FizxBuzz")
    }else{
-     if (i % 3 === 0) {
+     if (i % 3 === 0){
        console.log('Fizz');
      }else if ( i % 5 === 0){
        console.log('Buzz');
