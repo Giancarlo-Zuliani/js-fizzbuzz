8206750e3b43df092402c566c934372db1128ccf
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-26 15:37:20 +0100

    graphic layout, sound and  github button added;

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 
 var nums = 100;
 
-for (i = 1 ; i <= nums ; i++){
-   if( i % 3 === 0 && i % 5 === 0 ){
-     console.log("FizxBuzz")
-   }else{
-     if (i % 3 === 0){
-       console.log('Fizz');
-     }else if ( i % 5 === 0){
-       console.log('Buzz');
-     }else{
-       console.log(i);
-     }
-   }
+var fizzSound = new Audio ('sound/fizz.mp3');
+
+var buzzSound = new Audio('sound/buzz.mp3');
+
+var list = document.getElementById('list');
+
+
+function delay(i){
+  setTimeout(function(){
+    if( i % 3 === 0 && i % 5 === 0 ){
+      console.log("FizzBuzz")
+      buzzSound.play();
+      list.innerHTML += "<li class='fizzbuzz'>" + "FizzBuzz" + "</li>"
+    }else{
+      if (i % 3 === 0){
+        fizzSound.play();
+        console.log('Fizz');
+        list.innerHTML += "<li class='fizz'>" + "Fizz" + "</li>"
+      }else if ( i % 5 === 0){
+        console.log('Buzz');
+        buzzSound.play();
+        list.innerHTML += "<li class='buzz'>" + "Buzz" + "</li>"
+      }else{
+        console.log(i);
+        list.innerHTML += "<li>" + i + "</li>"
+              }
+    }
+  }, i * 300)
 };
+
+
+document.getElementById('btn').addEventListener('click',function(){
+  for (i = 1 ; i <= nums ; i++){
+    delay(i);
+}});
